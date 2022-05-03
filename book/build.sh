173eb06956bef6a21f9b0b37fc0186b8bbcd8cd5
Matthew Cengia <mattcen@mattcen.com>
2022-05-03 19:16:01 +1000

    Don't need terminal for building book

--- book/build.sh
+++ book/build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --rm -it -v "$(pwd)"/src:/book/src -v "$(pwd)"/book:/book/book peaceiris/mdbook:v0.4.18 build
+docker run --rm -v "$(pwd)"/src:/book/src -v "$(pwd)"/book:/book/book peaceiris/mdbook:v0.4.18 build
